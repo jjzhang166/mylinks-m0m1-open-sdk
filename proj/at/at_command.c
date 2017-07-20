2a872069094a24f68eab6ee0812b15156026a134
mqlinks <xuxubin@mqlinks.com>
2017-07-20 21:29:23 +0800

    Daniel-Mylinks
    解决AT库无法加载用户自定义AT的bug

--- proj/at/at_command.c
+++ proj/at/at_command.c
@@ -49,17 +49,33 @@
 
 sdk_param g_atParam;
 
+//AT查询，此函数将在串口输入AT+TEST执行
+//下例中，执行结果为+ok=Mylinks
 void at_exeCmdCtest(uint8_t id){
-
+	at_backOkHead;
+	uart0_sendStr("Mylinks");
+	at_backTail;
+	return;
 }
 
-void at_setupCmdtest(uint8_t id, char *pPara){
 
+//AT设置功能，此函数将在串口输入AT+TEST=Mylinks执行
+//当输入AT+TEST=mylinks,*pPara指向"Mylinks",即pPara="Mylinks"
+//用户可以自行进行处理,以下例子实现了:
+//当AT指令输入AT+Mylinks时，串口回复:+ok,否者输出:+ERR=-1
+void at_setupCmdtest(uint8_t id, char *pPara){
+	if(!strcmp(pPara,"Mylinks")){
+		at_backOk;
+	}else{
+		at_backError;
+		uart0_sendStr("-1");
+	}
+	return;
 }
 
 at_funType at_test[]={
 	{"TEST", 4, at_setupCmdtest, at_exeCmdCtest},
-	//user at funtion over
+	//自定义的AT指令，必须以以下方式结尾
 	{NULL, 0, NULL, NULL},
 };
 
