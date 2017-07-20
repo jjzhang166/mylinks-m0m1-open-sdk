1cdff6b233faf5904c99ba57cd3ad87c69840b18
mqlinks <xuxubin@mqlinks.com>
2017-07-20 21:39:45 +0800

    Daniel-Mylinks
    at demo注释

--- proj/at/at_command.c
+++ proj/at/at_command.c
@@ -38,19 +38,12 @@
 #include <minifs/mfs.h>
 #endif
 
-#define TEST_UART   0//Lynx UART1
-#define uart_log(...)                       serial_printf(__VA_ARGS__)
-#define UART_RECV_TIMEOUT                   0
-#define UART_ONE_PACKAGE_LENGTH             1024
-#define UART_BAUD_RATE                      115200
-
-
-
+#define UART0   0//Lynx UART1
 
 sdk_param g_atParam;
 
-//AT查询，此函数将在串口输入AT+TEST执行
-//下例中，执行结果为+ok=Mylinks
+/*AT查询，此函数将在串口输入AT+TEST执行
+下例中，执行结果为+ok=Mylinks */
 void at_exeCmdCtest(uint8_t id){
 	at_backOkHead;
 	uart0_sendStr("Mylinks");
@@ -59,10 +52,10 @@ void at_exeCmdCtest(uint8_t id){
 }
 
 
-//AT设置功能，此函数将在串口输入AT+TEST=Mylinks执行
-//当输入AT+TEST=mylinks,*pPara指向"Mylinks",即pPara="Mylinks"
-//用户可以自行进行处理,以下例子实现了:
-//当AT指令输入AT+Mylinks时，串口回复:+ok,否者输出:+ERR=-1
+/*AT设置功能，此函数将在串口输入AT+TEST=Mylinks执行
+  当输入AT+TEST=mylinks,*pPara指向"Mylinks",即pPara="Mylinks"
+  用户可以自行进行处理,以下例子实现了:
+  当AT指令输入AT+Mylinks时，串口回复:+ok,否者输出:+ERR=-1 */
 void at_setupCmdtest(uint8_t id, char *pPara){
 	if(!strcmp(pPara,"Mylinks")){
 		at_backOk;
@@ -106,12 +99,8 @@ int app_main(void)
 void uart_init(void)
 {
 	int threshold = 0;	
-	//g_atParam.ur_param.baudrate
-
-//	serial_printf("baud:%d,par:%d,stop:%d\n",baudrate_select(g_atParam.ur_param.baudrate),g_atParam.ur_param.parity,g_atParam.ur_param.stopbits);
-//	serial_conf(7, 0, 1, TEST_UART,threshold );
-	serial_conf(baudrate_select(g_atParam.ur_param.baudrate), g_atParam.ur_param.parity, g_atParam.ur_param.stopbits, TEST_UART,threshold );
-	serial_init((int)TEST_UART);
+	serial_conf(baudrate_select(g_atParam.ur_param.baudrate), g_atParam.ur_param.parity, g_atParam.ur_param.stopbits, UART0,threshold );
+	serial_init((int)UART0);
 
 #ifdef CONFIG_UR_FLOW_CONTROL
 	pin_mode(CONFIG_UR_CTS, 0);
@@ -124,6 +113,7 @@ void uart_init(void)
 
 
 void user_init(void){
+	//串口设置功能
 	uart_init();
 	//at_test:用户也自定义at指令
 	at_cmd_init(at_test);
@@ -132,19 +122,25 @@ void user_init(void){
 	if(mfmount(FS1_FLASH_ADDR) == RES_OK){
 		//当连接AP时，可以通过下面的网页来打开内置网页
 		dnsserver_init("set.mqlinks.com");
+		//启动内置web服务器
 		webserver_init();
 	}
 	for(;;)
 	{
-		//此函数必须存在
+		//AT系统中WIFI断开后的重连接
 		wifi_reset_proc();
-		//此函数必须存在
+		//AT系统中两路socket的重连接
 		at_net_proc();
 #ifdef CONFIG_OMNICONFIG
+		//smartconfig成功后回复APP
 		omni_state_connecting();
 #endif
+		//tcp服务器超时断开未响应
 		tcp_server_timeout_proc();
-		//此函数必须存在
+		/*GPIO检测功能,当GPIO6按下后会进入smartconfig功能
+		  !注意:如果些函数不使用，需要用sys_msleep(100);来代替
+		  否则user_init任务会僵死
+		*/
 		mylinks_gpio_proc();
 	}
 	return;
