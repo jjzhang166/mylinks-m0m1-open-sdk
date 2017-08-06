43bcaa45de5a0e216f488bceb7a68ec38c2712e7
mqlinks <xuxubin@mqlinks.com>
2017-08-06 15:21:56 +0800

    Daniel-Mylinks
    1.在mqtt代码中增加tls连接的实例

--- proj/mqtt/main.c
+++ proj/mqtt/main.c
@@ -34,6 +34,9 @@
 #include <net_api.h>
 #endif
 #include <user_config.h>
+#if defined(CONFIG_MINIFS)
+#include <minifs/mfs.h>
+#endif
 #include <mylinks_wifi.h>
 #include <MQTTClient.h>
 /*=============================================================================+
@@ -44,9 +47,12 @@
 
 /*----------------------------------------------------------------*/
 /**
- * 此例中使用没有无SSL连接方式,请在查看代码前做以下工作:
- * 将include文件夹中的“#define CONFIG_AXTLS 1”注释
- * make lib，重新编译mqtt.a静态库
+ * 此例中使用没有无SSL连接方式,请在查看代码前做以下工作
+ 注意:
+   当使用tls时:将include文件夹中lib_autoconf.h中“#define CONFIG_AXTLS 1”选中
+   当取消tls时:将include文件夹中lib_autoconf.h中“#define CONFIG_AXTLS 1”注释
+ * 每次重新编译lib_autoconf.h,都需要重新编译静态库
+ * make lib，重新编译mqtt.a和axtls.a静态库
  * make mqtt进行例程编译，生成mqtt.img文件
  */
 /*----------------------------------------------------------------*/
@@ -165,8 +171,39 @@ int MQTT_init(void)
 {
 	int rc = 0;
 	FreeRTOS_NetworkInit(&n);
+#if defined(CONFIG_AXTLS)
+	struct minifs root_ca_fs;
+	uint8_t *root_ca = NULL; 
+	struct minifs client_cert_fs;
+	uint8_t *client_cert = NULL;
+	struct minifs private_key_fs;
+	uint8_t *private_key = NULL;
+
+	//读取root_ca.crt内容
+	mfopen(&root_ca_fs,"root_ca.crt");
+	root_ca = (uint8_t *)malloc(mftell(&root_ca_fs));
+	mfread(&root_ca_fs,root_ca,mftell(&root_ca_fs));
+
+	//读取client_cert.crt内容
+	mfopen(&client_cert_fs,"client_cert.crt");
+	client_cert = (uint8_t *)malloc(mftell(&client_cert_fs));
+	mfread(&client_cert_fs,client_cert,mftell(&client_cert_fs));
+
+
+	//读取private_key.crt内容
+	mfopen(&private_key_fs,"private_key.crt");
+	private_key = (uint8_t *)malloc(mftell(&private_key_fs));
+	mfread(&private_key_fs,private_key,mftell(&private_key_fs));
+	getUTCtime("cn.pool.ntp.org");
+	rc = FreeRTOS_NetworkConnect(&n, "example.iot.us-east-1.amazonaws.com", 8883, root_ca, mftell(&root_ca_fs), client_cert, mftell(&client_cert_fs), private_key, mftell(&private_key_fs));
+	free(root_ca);
+	free(client_cert);
+	free(private_key);
+
+#else
 	//只需要填写MQTT协议的服务器IP或者域名，端口号。
 	rc = FreeRTOS_NetworkConnect(&n, "app.mqlinks.com", 1883, 0, 0, 0, 0, 0, 0);
+#endif
 	if (rc < 0) {
 		rc = FAILURE;
 		goto exit;
@@ -250,8 +287,8 @@ exit:
 	return;
 }
 
-#define SSID "amily"
-#define PWD	 "xu841007"
+#define SSID "mylinks"
+#define PWD	 "welcometomylinks"
 
 
 
@@ -274,7 +311,10 @@ void user_init(void){
 		strcpy(s.password,PWD);
 		wifi_station_set_config(&s);
 	}
-
+#if defined(CONFIG_AXTLS)
+	//当使用tls连接的时候，使用使用mini文件系统保存认证文件
+	mfmount(FS1_FLASH_ADDR);
+#endif
 	xTaskCreate(mqttclient, "mqtt", TASK_HEAP_LEN, 0, 5, NULL);
 	return;
 }
